efc26244edb43c8d282ff080186b13c09b6260d5
Piotr <piotrwc49@gmail.com>
2023-04-27 21:30:37 +0200

    working menu

--- app.py
+++ app.py
@@ -1,16 +1,34 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFrame, QWidget, QStackedWidget
 import sys
 
 
 class App(QMainWindow):
     def __init__(self):
         super(App, self).__init__()
-        uic.loadUi("app_ui.ui", self)
+        uic.loadUi("ui/app_ui.ui", self)
         self.setWindowTitle("app")
+
+        # pages
+        self.stack = self.findChild(QStackedWidget, "stackedWidget")
+        # menu buttons
+        self.init_pages()
 
         self.show()
 
+    def init_pages(self):
+        self.home = self.findChild(QPushButton, "home_button").clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.findChild(QWidget, "page_1")))
+        self.keylogger = self.findChild(QPushButton, "keylogger_button").clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.findChild(QWidget, "page_2")))
+        self.ransomware = self.findChild(QPushButton, "ransomware_button").clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.findChild(QWidget, "page_3")))
+        self.worm = self.findChild(QPushButton, "worm_button").clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.findChild(QWidget, "page_4")))
+        self.wifi = self.findChild(QPushButton, "wifi_button").clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.findChild(QWidget, "page_5")))
+        self.zip = self.findChild(QPushButton, "zip_button").clicked.connect(
+            lambda: self.stack.setCurrentWidget(self.findChild(QWidget, "page_6")))
 
 app = QApplication(sys.argv)
 win = App()
