973b4c398010b896a9c51fd927beb641b57a8b6c
Piotr <piotrwc49@gmail.com>
2023-04-28 15:55:09 +0200

    ransomware, keylogger, wifi and zip UI done

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QFrame, QWidget, QStackedWidget
 import sys
 
+# to open designer
+# qt5-tools designer
 
 class App(QMainWindow):
     def __init__(self):
@@ -12,11 +14,6 @@ class App(QMainWindow):
         # pages
         self.stack = self.findChild(QStackedWidget, "stackedWidget")
         # menu buttons
-        self.init_pages()
-
-        self.show()
-
-    def init_pages(self):
         self.home = self.findChild(QPushButton, "home_button").clicked.connect(
             lambda: self.stack.setCurrentWidget(self.findChild(QWidget, "page_1")))
         self.keylogger = self.findChild(QPushButton, "keylogger_button").clicked.connect(
@@ -29,6 +26,36 @@ class App(QMainWindow):
             lambda: self.stack.setCurrentWidget(self.findChild(QWidget, "page_5")))
         self.zip = self.findChild(QPushButton, "zip_button").clicked.connect(
             lambda: self.stack.setCurrentWidget(self.findChild(QWidget, "page_6")))
+
+        # ransomware
+        self.r_stack = self.findChild(QStackedWidget, "ransom_stack")
+        self.r_stack.setCurrentWidget(self.findChild(QWidget, "ransom_1"))  # set to first page
+        self.r_1 = self.findChild(QPushButton, "ransom_bt__1").clicked.connect(self.ransom_start)
+        self.r_2 = self.findChild(QPushButton, "ransom_bt__2").clicked.connect(self.ransom_keys)
+        self.r_3 = self.findChild(QPushButton, "ransom_bt__3").clicked.connect(self.ransom_send)
+        self.r_4 = self.findChild(QPushButton, "ransom_bt__4").clicked.connect(self.ransom_run)
+        self.r_5 = self.findChild(QPushButton, "ransom_bt__5").clicked.connect(self.ransom_decrypt)
+        self.r_6 = self.findChild(QPushButton, "ransom_bt__6").clicked.connect(self.ransom_return)
+
+        self.show()
+
+    def ransom_start(self):
+        self.r_stack.setCurrentWidget(self.findChild(QWidget, "ransom_2"))
+
+    def ransom_keys(self):
+        self.r_stack.setCurrentWidget(self.findChild(QWidget, "ransom_3"))
+
+    def ransom_send(self):
+        self.r_stack.setCurrentWidget(self.findChild(QWidget, "ransom_4"))
+
+    def ransom_run(self):
+        self.r_stack.setCurrentWidget(self.findChild(QWidget, "ransom_5"))
+
+    def ransom_decrypt(self):
+        self.r_stack.setCurrentWidget(self.findChild(QWidget, "ransom_6"))
+
+    def ransom_return(self):
+        self.r_stack.setCurrentWidget(self.findChild(QWidget, "ransom_1"))
 
 app = QApplication(sys.argv)
 win = App()
